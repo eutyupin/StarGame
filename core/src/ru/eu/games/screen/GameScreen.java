e0b80fcb3767ba5368b81e18d621b086313e6e8b
eutyupin <eu-1982@yandex.ru>
2021-10-25 13:19:14 +0300

    SomeConmmit

---
+++ core/src/ru/eu/games/screen/GameScreen.java
@@ -0,0 +1,91 @@
+package ru.eu.games.screen;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.math.Vector2;
+
+import ru.eu.games.base.BaseScreen;
+import ru.eu.games.math.Rect;
+import ru.eu.games.sprite.Background;
+import ru.eu.games.sprite.Star;
+
+public class GameScreen extends BaseScreen {
+
+    private static final int STAR_COUNT = 64;
+
+    private TextureAtlas atlas;
+    private Texture bg;
+    private Background background;
+
+    private Star[] stars;
+
+    @Override
+    public void show() {
+        super.show();
+        atlas = new TextureAtlas("textures/mainAtlas.tpack");
+        bg = new Texture("textures/bg.png");
+        background = new Background(bg);
+        stars = new Star[STAR_COUNT];
+        for (int i = 0; i < stars.length; i++) {
+            stars[i] = new Star(atlas);
+        }
+    }
+
+    @Override
+    public void render(float delta) {
+        super.render(delta);
+        update(delta);
+        draw();
+    }
+
+    @Override
+    public void resize(Rect worldBounds) {
+        super.resize(worldBounds);
+        background.resize(worldBounds);
+        for (Star star : stars) {
+            star.resize(worldBounds);
+        }
+    }
+
+    @Override
+    public void dispose() {
+        super.dispose();
+        bg.dispose();
+        atlas.dispose();
+    }
+
+    @Override
+    public boolean keyDown(int keycode) {
+        return super.keyDown(keycode);
+    }
+
+    @Override
+    public boolean keyUp(int keycode) {
+        return super.keyUp(keycode);
+    }
+
+    @Override
+    public boolean touchDown(Vector2 touch, int pointer, int button) {
+        return super.touchDown(touch, pointer, button);
+    }
+
+    @Override
+    public boolean touchUp(Vector2 touch, int pointer, int button) {
+        return super.touchUp(touch, pointer, button);
+    }
+
+    private void update(float delta) {
+        for (Star star : stars) {
+            star.update(delta);
+        }
+    }
+
+    private void draw() {
+        batch.begin();
+        background.draw(batch);
+        for (Star star : stars) {
+            star.draw(batch);
+        }
+        batch.end();
+    }
+}
